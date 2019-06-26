359e6eed8b7eab3a1e80b850190cc6fee4b4e5f2
Baha El Kassaby <baha.elkassaby@jax.org>
2019-06-26 14:32:34 -0400

    Do not use browser name to save test results

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function (config) {
       outputDir: 'test-reports', // results will be saved as $outputDir/$browserName.xml
       outputFile: 'junit-report.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
       suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true, // add browser name to report and classes names
+      useBrowserName: false, // add browser name to report and classes names
       nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
       classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
       properties: {} // key value pair of properties to add to the section of the report
