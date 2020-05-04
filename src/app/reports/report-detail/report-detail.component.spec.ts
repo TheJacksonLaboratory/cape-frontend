3e2500f2f26210fb3d131f724eb6d9cd5ea36db2
Baha El Kassaby <Baha.ElKassaby@jax.org>
2020-05-04 11:37:06 -0400

    Fix compile error

--- src/app/reports/report-detail/report-detail.component.spec.ts
+++ src/app/reports/report-detail/report-detail.component.spec.ts
@@ -6,10 +6,15 @@ import { ReportDetailComponent } from './report-detail.component';
 import { Report } from 'src/app/_models/report';
 import { ReportsService } from 'src/app/_services';
 
-const report = new Report(1, 'Report One', 'Alice Apple', 'Dignissim posuere vestibulum eget, sollicitudin rutrum. Justo, malesuada. Adipisicing netus et malesuada fames est. Consectetuer. Ornare. Etiam faucibus. Aliquam commodo lacus sit amet, nec, risus. Scelerisque eget, urna. Sollicitudin tortor at nulla ut accumsan, neque id turpis egestas.');
+let report: Report;
 
 class MockReportService {
   getReport(): Observable<Report> {
+      report = new Report();
+      report.id = 1;
+      report.title = 'Report One';
+      report.author = 'Alice Apple';
+      report.description = 'Dignissim posuere vestibulum eget, sollicitudin rutrum. Justo, malesuada. Adipisicing netus et malesuada fames est. Consectetuer. Ornare. Etiam faucibus. Aliquam commodo lacus sit amet, nec, risus. Scelerisque eget, urna. Sollicitudin tortor at nulla ut accumsan, neque id turpis egestas.';
       return of(report);
   }
 }
@@ -35,7 +40,12 @@ describe('ReportSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ReportDetailComponent);
     component = fixture.componentInstance;
-    component.report = new Report(1, 'Report One', 'Alice Apple', 'Dignissim posuere vestibulum eget, sollicitudin rutrum. Justo, malesuada. Adipisicing netus et malesuada fames est. Consectetuer. Ornare. Etiam faucibus. Aliquam commodo lacus sit amet, nec, risus. Scelerisque eget, urna. Sollicitudin tortor at nulla ut accumsan, neque id turpis egestas.');
+    component.report = new Report();
+    component.report.id = 1;
+    component.report.title = 'Report One';
+    component.report.author = 'Alice Apple';
+    component.report.description = 'Dignissim posuere vestibulum eget, sollicitudin rutrum. Justo, malesuada. Adipisicing netus et malesuada fames est. Consectetuer. Ornare. Etiam faucibus. Aliquam commodo lacus sit amet, nec, risus. Scelerisque eget, urna. Sollicitudin tortor at nulla ut accumsan, neque id turpis egestas.';
+
     reportsService = TestBed.get(ReportsService);
     fixture.detectChanges();
   });
