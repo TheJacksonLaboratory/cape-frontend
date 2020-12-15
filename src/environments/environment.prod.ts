026987789b5f782078efbe16379d34be32c9ae62
Baha El Kassaby <Baha.ElKassaby@jax.org>
2020-12-14 22:29:43 -0500

    Remove unused component and put correct prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
   production: true,
   FILE_URL: 'http://bhcape01.jax.org:8080',
   V1_URL: API_URL + '/v1',
-  AUTH_URL: API_URL + '/auth/',
-  DATA_FILE_URL: API_URL + '/datafiles/',
-  PARAMETERS_URL: API_URL + '/parameters/',
-  USER_URL: API_URL + '/user/',
-  JOB_URL: API_URL + '/jobs/',
-  REPORT_URL: API_URL + '/reports/'
+  AUTH_URL: API_URL + '/auth',
+  DATA_FILE_URL: API_URL + '/datafiles',
+  PARAMETERS_URL: API_URL + '/parameters',
+  USER_URL: API_URL + '/user',
+  JOB_URL: API_URL + '/jobs',
+  REPORT_URL: API_URL + '/reports'
 };
